45ff138884e6159b0a274712bd637540a15d741f
abdalkalk <eng.abdalkalk@gmail.com>
2017-03-25 19:25:32 +0300

    add the code of api

---
+++ code/project.js
@@ -0,0 +1,19 @@
+var url="https://api.github.com/users/abdalkalk/repos?per_page=5"
+
+     
+$(document).ready(function(){
+	$("#a").click(function(){
+    $.get(url, function(data){
+        $.each(data,function(key,v){
+
+                 console.log(v.name);
+
+        })
+            });
+});
+
+})
+
+
+
+
